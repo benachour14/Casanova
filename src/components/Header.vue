deb71c6a82745a6cdf6ec47c7a1782f28ad0531d
Anass <anass.benachour14@gmail.com>
2024-05-19 14:47:01 +0200

    add Landing page, fix the project structure

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Button from "./ui/Button.vue"
+</script>
+
+
+<template>
+
+        <div class="relative">
+            <img src="../assets/backgroundImage.jpg" class="bg-cover absolute top-0 left-0" />
+
+            <div class="absolute w-4/12 left-44 top-56">
+                <p class="text-4xl font-bold leading-snug">Plaisirs <span class="text-amber-500">italiens</span>  à chaque bouchée, seulement chez <span class="text-amber-500">Casanova</span> </p>
+            <p class="mt-4 text-sm">
+                Bienvenue chez Casanova, où l'Italie rencontre votre assiette. Découvrez nos spécialités italiennes préparées avec passion par notre chef. Plongez dans une expérience culinaire inoubliable. À Casanova, chaque plat raconte une histoire d'amour avec la cuisine italienne.            
+            </p>
+
+            <Button class="mt-4">Plus d'informations</Button>
+            </div>
+        </div>
+</template>
+
+
+<style>
+</style>
---
+++ src/components/ui/Button.vue
@@ -0,0 +1,6 @@
+<template>
+    <button
+        class="bg-amber-500 hover:bg-amber-400 text-white font-bold py-2 px-4 rounded-lg shadow-lg shadow-matisse-500/50">
+        <slot />
+    </button>
+</template>
