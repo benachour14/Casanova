d1944e38ae7915eb8cdad455f93f8fd5b132aa88
Anass <anass.benachour14@gmail.com>
2024-05-25 17:28:17 +0200

    add footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,32 @@
 <template>
-    <footer>
-        <h1>Footer</h1>
-    </footer>
+   
+
+<footer>
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+
+      <hr class="mb-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-8" />
+      <div class="sm:flex sm:items-center sm:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Gildedigital</a>. All Rights Reserved.
+          </span>
+          <div class="flex mt-4 sm:justify-center sm:mt-0">
+              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <i class="fa-brands fa-facebook text-3xl cursor-pointer mr-4"></i>
+                  <span class="sr-only">Facebook page</span>
+              </a>
+              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <i class="fa-brands fa-instagram text-3xl cursor-pointer mr-6"></i>
+                  <span class="sr-only">Instagram page</span>
+              </a>
+          </div>
+      </div>
+    </div>
+</footer>
+
 </template>
+
+<style scoped >
+footer {
+  position: relative;
+  z-index: 10;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import FooterComponent from '../components/FooterComponent.vue';
 import Header from '../components/Header.vue';
 
 </script>
 
 <template>
   <Header />
+  <FooterComponent />
 
 </template>
